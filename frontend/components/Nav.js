7b79c9e58e8b6ebef32ca136757ece59be65e3bf
Eric Nation <eric.nation@gmail.com>
2019-03-04 21:41:03 -0700

    first commit

---
+++ frontend/components/Nav.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
+
+const Nav = () => {
+  return (
+    <NavStyles>
+      <Link href="/item">
+        <a>Item</a>
+      </Link>
+      <Link href="/sell">
+        <a>Sell</a>
+      </Link>
+      <Link href="/signup">
+        <a>Signup</a>
+      </Link>
+      <Link href="/orders">
+        <a>Orders</a>
+      </Link>
+      <Link href="/account">
+        <a>Account</a>
+      </Link>
+    </NavStyles>
+  );
+};
+
+export default Nav;
